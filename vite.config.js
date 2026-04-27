b40a17a4e8fcce4d4a366d6201305324567c77f5
Larissa Batista dos Santos <larissabatista0704@gmail.com>
2026-04-27 12:23:05 -0300

    update config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,12 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css', 
+                'resources/js/app.js',
+                'resources/css/movies.css',
+                'resources/js/movies.js'
+            ],
             refresh: true,
         }),
         tailwindcss(),
